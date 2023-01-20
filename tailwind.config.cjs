d23dbc3557a469bf8e62b5f3536b80bd59371eba
JAN URBAN NETO <jan.urbanneto@gmail.com>
2023-01-20 09:41:57 -0300

    table and header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-    './index.html'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.tsx", "./index.html"],
+
+	theme: {
+		extend: {
+			colors: {
+				nlwbg: "#09000A",
+			},
+
+            gridTemplateRows: {
+                7: 'repeat(7, minmax(0, 1fr))'
+            },
+		},
+	},
+	plugins: [],
+};
